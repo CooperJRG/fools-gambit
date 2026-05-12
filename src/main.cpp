cb1e8ce2057511ab433cad7a66699a0ed4c573a5
cooperjrg <cooperjgilkey@gmail.com>
2026-05-12 15:55:02 -0500

    stub main.py

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+// Stub main file for fools gambit. This will be the entry point for the application.
+#include <iostream>
+
+int main() {
+    std::cout << "Fool's Gambit stub main\n";
+    return 0;
+}
